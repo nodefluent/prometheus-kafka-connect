d2778d53a5de1bb80d079ab5b1cb3c35cb338ea2
rjmasikome <rjmasikome@gmail.com>
2017-10-20 14:41:55 +0200

    Make register local, and refactor router from config

--- example/config.js
+++ example/config.js
@@ -1,46 +1,33 @@
 "use strict";
 
-const path = require("path");
-const express = require("express");
-const { register } = require("prom-client");
-const debug = require("debug")("nkc:prom:config");
-const router = express.Router();
-
-router.use("/metrics", (req, res) => {
-  res.set('Content-Type', register.contentType);
-  res.end(register.metrics());
-})
-
 const config = {
-    kafka: {
-        noptions: {
-            "debug": "all",
-            "metadata.broker.list": process.env["KAFKA_HOST"] || "localhost:9092",
-            "group.id": "n-test-group-9092",
-            "enable.auto.commit": false,
-            "event_cb": true
-        }
+  kafka: {
+    noptions: {
+      "debug": "all",
+      "metadata.broker.list": process.env["KAFKA_HOST"] || "localhost:9092",
+      "group.id": "n-test-group-9092",
+      "enable.auto.commit": false,
+      "event_cb": true
+    }
+  },
+  topic: "kc-prom-1",
+  partitions: 1,
+  maxTasks: 1,
+  pollInterval: 250,
+  produceKeyed: true,
+  produceCompressionType: 0,
+  connector: {
+    options: {
+      job: "promclient_job",
+      additionalLabels: ["marketId"],
+      logging: () => {}
     },
-    topic: "kc-prom-1",
-    partitions: 1,
-    maxTasks: 1,
-    pollInterval: 250,
-    produceKeyed: true,
-    produceCompressionType: 0,
-    connector: {
-        options: {
-            job: "promclient_job",
-            additionalLabels: ["method"],
-            logging: () => {}
-        },
-    },
-    http: {
-        port: 3149,
-        middlewares: [router]
-    },
-    enableMetrics: true
+  },
+  http: {
+    port: 3149,
+    middlewares: []
+  },
+  enableMetrics: true
 };
-
-debug(config);
 
 module.exports = config;
